a301f45db450d2ad740a57f3edf817a5703f1bd7
Cyril Safin <kirillsafin@nlstar.com>
2017-06-05 00:21:40 +0400

    4 программы: - замена местами четных\нечетных букв - нахохдение минимального времени(надо доработать) - вывод инкремента в таблицу - поиск заглавных букв в строке

---
+++ Lessons/Change 2in1 letters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Change_2in1_letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine().ToUpper();
+            string ans = string.Empty;
+
+            if (str.Length % 2 == 0)
+            {
+                for (int i = 0; i < str.Length; i += 2)
+                { ans += str[i + 1].ToString() + str[i].ToString(); }
+            }
+            else
+            {
+                for (int i = 1; i < str.Length; i += 2)
+                { ans += str[i].ToString() + str[i-1].ToString(); }
+                ans += str[str.Length-1];
+            }
+
+            Console.Write(ans);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lessons/Is Upper in string/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_Upper_in_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string ans=string.Empty;
+            
+            char[] array = str.ToArray();
+
+            foreach (var x in array)
+            {
+                if (65 <= x && x <= 90)
+                {
+                    ans += x;
+                }
+            }
+            Console.Write(ans);
+            Console.ReadKey();
+        }
+    }
+}
