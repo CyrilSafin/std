dd4467994387a2da62a64dab186f68c0ac8f6b49
Cyril Safin <kirillsafin@nlstar.com>
2017-06-06 19:50:15 +0400

    первая версия не работает с двойными # и не работает в IDE

---
+++ Lessons/Gravity/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gravity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int width = int.Parse(inputs[0]);
+            int height = int.Parse(inputs[1]);
+
+            char[,] arr = new char[width, height];
+            for (int j = 0; j < width; j++)
+            {
+                string line = Console.ReadLine();
+                
+                for (int i = 0; i < height; i++)
+                {
+                    arr[i, j] = line[i];//.GetTypeCode();//.ToString();
+                }
+            }
+            
+
+            for (int i = 0; i < width; i++)
+            {
+                bool exit = true;
+                do
+                {
+                    for( int j=0;j<height-1;j++)
+                    {
+                        if (arr[i, j] == '#')
+                        {
+                            if (arr[i, j + 1] == '.')                             { arr[i, j] = '.'; arr[i, j + 1] = '#';}
+                        }
+                    }
+                    exit = false;
+                }
+                while (exit == true);
+            }
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Console.Write(arr[j,i]);
+                }
+                Console.Write('\n');
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
